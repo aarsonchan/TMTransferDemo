5ce374a04db763ad33e1171fa52062ab686329eb
Aarson Chan <aarsonchan@gmail.com>
2018-06-25 01:03:19 +0800

    Delete Derived Data

--- DerivedData/TMTransferDemo/Build/Intermediates.noindex/Pods.build/Debug-iphonesimulator/Pods-TMTransferDemo.build/DerivedSources/Pods_TMTransferDemo_vers.c
+++
@@ -1,5 +0,0 @@
- extern const unsigned char Pods_TMTransferDemoVersionString[];
- extern const double Pods_TMTransferDemoVersionNumber;
-
- const unsigned char Pods_TMTransferDemoVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:Pods_TMTransferDemo  PROJECT:Pods-1" "\n";
- const double Pods_TMTransferDemoVersionNumber __attribute__ ((used)) = (double)1.;
